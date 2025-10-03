5c689896bdcc08fbcb9d419655626a5d772b1796
armita-04 <marzieh.seyedi.04@gmail.com>
2025-10-03 21:30:35 +0330

    working on backend

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+import dbConnection from "./util/db.js";
+dotenv.config();
+
+
+// server
+const PORT = process.env.PORT;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+    dbConnection();
+
+})
