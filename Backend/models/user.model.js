a9e88018028b4a69a2c6d03df8ca7d0f2f169cae
armita-04 <marzieh.seyedi.04@gmail.com>
2025-10-08 13:25:01 +0330

    worked on product model and controller

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
-
-const userSchema = mongoose.Schema({
+import bcrypt from "bcryptjs";
+const userSchema = mongoose.Schema(
+  {
     name: {
       type: String,
       require: true,
@@ -38,6 +39,18 @@ const userSchema = mongoose.Schema({
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);  
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
